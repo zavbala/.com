93c2a54273c803d34fb3f697b1fdd6f0fc89aecc
username <sample@example.com>
2022-09-12 15:16:53 -0600

    Add: Splitbee Analytics / Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://zavbala.com",
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [tailwind(), react(), sitemap(), partytown()]
 });
