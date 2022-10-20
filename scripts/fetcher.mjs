a8045706d87d32a455bb76f0e17e8bccbdc7ce08
username <sample@example.com>
2022-10-19 20:49:01 -0600

    fix: async/await issue

--- scripts/fetcher.mjs
+++ scripts/fetcher.mjs
@@ -2,31 +2,37 @@ import { writeFileSync } from 'fs';
 
 const BLACKLIST = ['zavbala', 'dotfiles', '.com', 'sargabot'];
 
-(async () => {
-  const res = await fetch(' https://api.github.com/users/zavbala/repos', {
+const fetcher = () => {
+  fetch(' https://api.github.com/users/zavbala/repos', {
     method: 'GET',
     headers: new Headers({
       Accept: 'application/vnd.github+json',
     }),
-  });
+  })
+    .then((response) => {
+      if (response.ok) return response.json();
+    })
+    .then((data) => {
+      const publicRepos = [];
 
-  const data = await res.json();
-  const publicRepos = [];
+      for (const project of data) {
+        if (!BLACKLIST.includes(project.name)) {
+          publicRepos.push({
+            title: project.name,
+            about: project.description,
+            date: project.updated_at,
+            ref: project.html_url,
+            size: project.size,
+          });
+        }
+      }
 
-  for (const project of data) {
-    if (!BLACKLIST.includes(project.name)) {
-      publicRepos.push({
-        title: project.name,
-        about: project.description,
-        date: project.updated_at,
-        ref: project.html_url,
-        size: project.size,
-      });
-    }
-  }
+      writeFileSync(
+        process.cwd() + '/data/Archive.json',
+        JSON.stringify(publicRepos, null, ' ')
+      );
+    })
+    .catch((error) => console.log(error));
+};
 
-  writeFileSync(
-    process.cwd() + '/data/Archive.json',
-    JSON.stringify(publicRepos, null, ' ')
-  );
-})();
+fetcher();
